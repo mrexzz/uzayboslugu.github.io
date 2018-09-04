c6083e02ab980b353c4cce4431074442a4a4bb37
uzayboslugu <42938722+uzayboslugu@users.noreply.github.com>
2018-09-04 03:11:12 +0300

    url havuz kontrol

---
+++ url-havuzu/urlara.py
@@ -0,0 +1,40 @@
+
+#   URL Kontürolü yapmadan önce urlhavuzu.txt isimli
+#   dosyanın güncel olduğuna dikkat ediniz.
+
+"""
+    urlara.py v0.1
+
+    Eklenmek istenen URL adresinin havuzda var yada
+    yok olduğunu anlamak icin basit bir arac. Amacı
+    eş URL ekleme sorununu ortadan kaldırmak. Ekleme
+    yapılacak URL havuzda mevcut degilse havuza eklenir.
+"""
+
+
+from sys import argv
+
+def ara(url):
+    durum = str()
+    with open('urlhavuz.txt','r') as dosya:
+        for i in dosya.readlines():
+            if i == url+'\n':
+                durum = False
+
+    if durum != False:
+        with open('urlhavuz.txt','a') as dosya:
+            dosya.write(url+'\n'); print('<url eklendi>')
+    
+ara(argv[1])
+
+
+"""
+    Kullanım sekilleri:
+
+    >_ python urlara.py url_adresi
+
+    >_ python /dosya/dizini/urlara.py url_adresi
+
+    Seklinde URL adresleri kontrol edilebilir yada
+    havuza URL eklenebilir.
+"""
